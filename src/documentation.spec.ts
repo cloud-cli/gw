8d264e7bd6cdcb1ff5daf874f9fee5cb5c09886c
Darlan Alves <me@darlanalv.es>
2021-08-08 23:21:40 +0200

    feat: use current dir for documentation and add styling to it

--- src/documentation.spec.ts
+++ src/documentation.spec.ts
@@ -6,12 +6,14 @@ describe('Documentation', () => {
     const request: any = {};
 
     const response: any = {
+      setHeader: jasmine.createSpy('setHeader'),
       writeHead: jasmine.createSpy('writeHead'),
       end: jasmine.createSpy('end'),
     };
 
     await docs.get(request, response);
 
+    expect(response.setHeader).toHaveBeenCalledWith('Content-Type', 'text/html');
     expect(response.writeHead).toHaveBeenCalledWith(200);
     expect(response.end).toHaveBeenCalledTimes(1);
 
--- src/documentation.ts
+++ src/documentation.ts
@@ -12,7 +12,14 @@ export class Documentation {
     const text = String(await readFile(join(this.folder, 'README.md')));
     const html = this.renderer.render(text);
 
+    response.setHeader('Content-Type', 'text/html');
     response.writeHead(200);
-    response.end(html);
+    response.end(`
+    <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/@tailwindcss/typography@0.4.x/dist/typography.min.css" />
+    <article class="prose lg:prose-xl m-auto py-12">
+    ${html}
+    </article>
+    `);
   }
 }
